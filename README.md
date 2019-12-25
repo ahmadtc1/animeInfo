b2fc3c14e2111943c06c3811b9f9218ed374a2a4
ahmadtc1 <ahmadtc2000@gmail.com>
2019-12-25 00:15:30 -0500

    Add templates for views Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# flaskApp
-flask App
+# AnimeInfo
+An application for obtaining more info about the animes of your choice
+Just enter the anime of your choice and hit submit! Let the facts roll in
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/home", methods=['GET'])
 def home():
-	return "Hello World"
+	return render_template("home.html")
+
+
+@app.route("/")
+def default():
+	return render_template("home.html")
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
